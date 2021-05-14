dfdc9199114d735e2a4ff50567e20c468437b89a
Fernando <ferjomagra@hotmail.com>
2021-05-14 12:00:45 -0500

    fourth commit

--- server/src/controllers/commits.controller.js
+++ server/src/controllers/commits.controller.js
@@ -1,116 +1,115 @@
 const commits_controller = {}
 const https = require('https')
 const fetch = require('node-fetch');
-
+const {Octokit} = require('@octokit/rest');
 
 const Commit = require('../models/commits')
 
+
+
+
 commits_controller.presentation = (req,res)=>{
 
-	/*const getinfo = await ferjomagra.request('GET /repos/Ferjomagra/FulltimeForce', {
-		owner: 'Ferjomagra',
-		repo: 'FulltimeForce'
-	})*/
-	res.render('presentation/index')
+  res.render('presentation/index')
 }
 
 commits_controller.getCommits = (req,res)=>{
 
-	Commit.find({}, function(err, commitList){
-		if(err){
-			res.redirect('/')
-			return
-		}
-		/*console.log(commitList)*/
-		
-		res.render('presentation/commits', {
-			commitList : commitList.reverse()
-		})
-	})
+  Commit.find({}, function(err, commitList){
+    if(err){
+      res.redirect('/')
+      return
+    }
+    /*console.log(commitList)*/
+    
+    res.render('presentation/commits', {
+      commitList : commitList.reverse()
+    })
+  })
 }
 
-commits_controller.saveCommits = (req,res)=>{
+commits_controller.saveCommits = async (req,res)=>{
 
     //Otra fecha
     var gDate = new Date();
-    //fecha
-    var year = gDate.getFullYear();
-    var month = gDate.getMonth()+1;
-    var dt = gDate.getDate();
+      //fecha
+      var year = gDate.getFullYear();
+      var month = gDate.getMonth()+1;
+      var dt = gDate.getDate();
 
-    if (dt < 10) {
-        dt = '0' + dt;
+      if (dt < 10) {
+          dt = '0' + dt;
+      }
+      if (month < 10) {
+          month = '0' + month;
+      }
+
+      var _date = year+'-' + month + '-'+dt;
+
+      //hour
+      var hours = gDate.getHours();
+      var minutes = gDate.getMinutes();
+      var ampm = hours >= 12 ? 'pm' : 'am';
+      hours = hours % 12;
+      hours = hours ? hours : 12; // the hour '0' should be '12'
+      minutes = minutes < 10 ? '0'+minutes : minutes;
+
+      var _strTime = hours + ':' + minutes + ' ' + ampm;
+
+      var nh = gDate.getHours();
+      var nm = gDate.getMinutes();
+      var ns = gDate.getSeconds();
+
+      if(nh < 10){
+          nh = '0' + nh;
+      }
+
+      if(nm < 10){
+          nm = '0' + nm;
+      }
+
+      if(ns < 10){
+          ns = '0' + ns;
+      }
+
+      var createdAt = _date + ' ' + nh + ':' + nm + ':' + ns;
+
+  /*let url = 'https://api.github.com/repos/ferjomagra/FulltimeForce';
+
+  let settings = {method: "get"};*/
+
+  const octokit = new Octokit();
+
+  
+  const getinfo = await octokit.request('GET /repos/Ferjomagra/FulltimeForce', {
+    owner: 'Ferjomagra',
+    repo: 'FulltimeForce'
+  })
+
+  const data = getinfo
+  /*console.log(data)*/
+
+  var commits = new Commit({
+    tdata: JSON.stringify(data),
+    name: JSON.stringify(data.data.name),
+    id_repo: JSON.stringify(data.data.id),
+    created_at: JSON.stringify(data.headers.date),
+    html_url: 'https://github.com/Ferjomagra/FulltimeForce',
+    api_url: 'https://api.github.com/repos/ferjomagra/FulltimeForce',
+    date: gDate,
+    current_date: createdAt
+  })
+  commits.save().then(
+    () =>{
+      res.redirect('/commits')
     }
-    if (month < 10) {
-        month = '0' + month;
+  ).catch(
+    (error) =>{
+      res.status(400).json({
+        error: error
+      })
     }
-
-    var _date = year+'-' + month + '-'+dt;
-
-    //hour
-    var hours = gDate.getHours();
-    var minutes = gDate.getMinutes();
-    var ampm = hours >= 12 ? 'pm' : 'am';
-    hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    minutes = minutes < 10 ? '0'+minutes : minutes;
-
-    var _strTime = hours + ':' + minutes + ' ' + ampm;
-
-    var nh = gDate.getHours();
-    var nm = gDate.getMinutes();
-    var ns = gDate.getSeconds();
-
-    if(nh < 10){
-        nh = '0' + nh;
-    }
-
-    if(nm < 10){
-        nm = '0' + nm;
-    }
-
-    if(ns < 10){
-        ns = '0' + ns;
-    }
-
-    var createdAt = _date + ' ' + nh + ':' + nm + ':' + ns;
-
-	let url = 'https://api.github.com/repos/ferjomagra/FulltimeForce';
-
-	let settings = {method: "get"};
-
-	fetch(url, settings)
-	.then(res => res.json())
-	.then((commit_json) =>{
-		var data = commit_json
-		
-		/*console.log(data)*/
-		/*res.send(commit_json)*/
-		var commits_data = {
-			tdata: JSON.stringify(data),
-			name: JSON.stringify(data.name),
-			id_repo: JSON.stringify(data.id),
-			created_at: JSON.stringify(data.created_at),
-			html_url: 'https://github.com/Ferjomagra/FulltimeForce',
-			api_url: 'https://api.github.com/repos/ferjomagra/FulltimeForce',
-			date: gDate,
-			current_date: createdAt
-		}
-
-		const newCommit = new Commit(commits_data)
-
-		/*console.log(newCommit)*/
-
-		newCommit.save(function(err){
-			if(!err){
-				res.redirect('/commits')
-				console.log(newCommit)
-			} else {
-				res.render('/')
-				console.log('No se guardo')
-			}
-		})
-	})
+  )
 }
 
 
--- server/src/models/commits.js
+++ server/src/models/commits.js
@@ -2,15 +2,14 @@ const mongoose = require('mongoose')
 const { Schema, model } = require('mongoose')
 
 const commitSchema = new Schema({
-	tdata: {type: String, required: true},
-	name: {type: String, required: true},
-	id_repo: {type:String, required: true},
-	html_url: {type:String, required: true},
-	api_url: {type: String, required: true},
-	created_at: {type: String, required: true},
-	date: {type: String, required: true},
-	current_date: {type: String, required: true}
-
+	tdata: {type: String},
+	name: {type: String},
+	id_repo: {type:String},
+	html_url: {type:String},
+	api_url: {type: String},
+	created_at: {type: String},
+	date: {type: String},
+	current_date: {type: String}
 })
 
 module.exports = mongoose.model('Commit', commitSchema)
