6809c5035b3f381d8be8327478b27dde09173ff3
Ferjomagra <35279648+Ferjomagra@users.noreply.github.com>
2021-07-01 09:07:52 -0500

    Update database.js

--- server/src/database.js
+++ server/src/database.js
@@ -4,7 +4,7 @@ mongoose.connect('mongodb://localhost/github_project',{
 	useUnifiedTopology: true,
 	useNewUrlParser: true
 })
-/*mongoose.connect('mongodb+srv://1FulltimeForce:0dSgm2q95L4ntIOq@cluster0.mlxkw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+/*mongoose.connect('',{
 	useUnifiedTopology: true,
 	useNewUrlParser: true
 })*/
